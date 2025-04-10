7e7f3b74e5c80a4496902d3cb721d824a2cb79ca
CraigIDent <dent.craig1@gmail.com>
2025-04-10 20:55:03 +0200

    slowed answer display to 1400ms

--- src/FlashcardApp.jsx
+++ src/FlashcardApp.jsx
@@ -176,7 +176,7 @@ const drawNextCard = () => {
     setShowAnswer(true);
     setTimeout(() => {
       setShowAnswer(false);
-    }, 1000);
+    }, 1400);
   };
 
   const renderPile = (pile, color, label, position) => (
