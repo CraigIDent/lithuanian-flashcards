2a49e69da5591c4aae567363300f1e9e0e777265
CraigIDent <dent.craig1@gmail.com>
2025-04-10 10:13:20 +0200

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  base: '/deutsche-flashcards/', // this line is key
   plugins: [react()],
 });
