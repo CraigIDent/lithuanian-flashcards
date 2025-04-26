4d6e235c0b210f86c1c091a3601fde03ca9b914e
CraigIDent <dent.craig1@gmail.com>
2025-04-26 22:32:44 +0200

    moved audio toggle

--- src/FlashcardApp.jsx
+++ src/FlashcardApp.jsx
@@ -336,7 +336,7 @@ export default function FlashcardApp() {
         </label>
       </div>
       {/* Audio toggle */}
-      <div className="absolute top-2 left-1/2 transform -translate-x-1/2 flex flex-col gap-2 items-center">
+      <div className="absolute top-2 right-4 flex flex-col gap-2 items-end">
         <span className="text-sm">Audio</span>
         <label className="relative inline-flex items-center cursor-pointer">
           <input
